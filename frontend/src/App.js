8d9684977063820e4483560d2091f14875c8714a
aaron.kwag <9592@daum.net>
2019-03-25 20:59:09 -0400

    create react app

---
+++ frontend/src/App.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+//import logo from './logo.svg';
+import { ReactComponent as Logo } from "./logo.svg";
+import styles from "./App.module.scss";
+ 
+class App extends Component {
+  render() {
+    return (
+      <>
+      <div className={styles.app}>
+        <header className={styles.appHeader}>
+          
+          <Logo />
+
+
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className={styles.appLink}
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+      <span>lalalala</span>
+      </>
+    );
+  }
+}
+
+export default App;
